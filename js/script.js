b7f2492ae62a5b60f060c4427a9ddf3f775ac832
Sandu00 <sandudavidalex@gmail.com>
2024-12-23 18:06:46 +0100

    added redirection in home button and link ref to all the navbar texts

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     navLinks.forEach(link => {
         link.addEventListener('click', event => {
             event.preventDefault();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            targetSection.scrollIntoView({ behavior: 'smooth' });
+            const targetUrl = link.getAttribute('href');
+            window.location.href = targetUrl;
         });
     });
 
